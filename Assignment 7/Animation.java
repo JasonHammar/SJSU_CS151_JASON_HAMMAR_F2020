5a7c74566ade4099e4f6f96a0c1ba9cc75b62bde
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2020-12-01 18:15:18 -0800

    Add files via upload

---
+++ Assignment 7/Animation.java
@@ -0,0 +1,7 @@
+
+
+public class Animation {
+	public static void main(String args[]) {
+		
+	}
+}
---
+++ Assignment 7/Calculator.java
@@ -0,0 +1,26 @@
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Calculator extends Application 
+{
+	
+	public static void Calculator() {
+		
+	}
+    
+    public static void main(String[] args) 
+    {
+        Calculator();
+    }
+}
